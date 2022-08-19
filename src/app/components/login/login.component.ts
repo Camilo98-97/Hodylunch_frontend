3a2b748f5c8fcd44f33c513f858d802455a1a6f7
Camilo98-97 <dacane1998@gmail.com>
2022-08-19 12:02:40 -0500

    componente login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) { 
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  ingresar() {
+    
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if (usuario == 'camilo' && password == '123') {
+      
+    } else {
+
+    }
+
+  }
+
+}
